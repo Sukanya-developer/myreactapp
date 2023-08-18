db64a54974e28fd01a46955dd60141c22ef80e8b
CORP\z013851 <sukanya.shanmugam@rntbci.com>
2023-08-18 19:52:20 +0530

    commit

---
+++ src/components/Home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../styles/Home.css'
+// rfce shortcut to create the contents
+function Home() {
+    const heading ="My First React Program"
+  return (
+    <div> <h1 id="header-text"><u>{heading}</u></h1>
+    <div><img align="left" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiy6854vkcOGz9KhzFD8qwkfdj1wZ2zd8t-KdBCKlqYw&s" alt="erfcdsc"></img>
+   
+   
+   
+    <p align="center">dvdfdfvf<br></br>
+    dfvdfvfdvfdvf<br></br>
+        kmxvfdvfdvfdv<br></br>
+    dfvfdvfdvdffffffffffffffffeeeeeeeeeeeer<br></br>
+    gbbbbbbbbbbbtrmnsc<br></br>
+    cdcewwwwwwwwwwwwwwwwwwwwdwdwwww<br></br>
+    </p></div></div>
+  )
+}
+
+export default Home
